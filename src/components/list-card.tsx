30ccc7feb028a0f9164b61998d988769f48962be
Milton Black <arziblack2@gmail.com>
2024-11-07 06:30:05 +0100

    feat: added delete modal to list cards...

--- src/components/list-card.tsx
+++ src/components/list-card.tsx
@@ -7,31 +7,63 @@ import {
   CardTitle,
 } from "./ui/card";
 import { Button } from "./ui/button";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 interface IListCard {
   title: string;
   description?: string;
+  button?: React.ReactNode;
 }
 
 const ListCard: React.FC<IListCard> = ({
   title,
   description,
+  button,
 }): React.JSX.Element => {
   return (
     <div className="w-full mb-2">
-      <Card className=" w-[600px]">
-        <CardHeader>
-          <CardTitle>{title}</CardTitle>
-        </CardHeader>
-        <CardContent>{description}</CardContent>
-        <CardFooter className="flex w-full justify-between my-2">
-          <Button variant="secondary" className="w-[100px] p-1">
-            view
-          </Button>
-          <Button variant="secondary" className="w-[100px] p-1">edit</Button>
-          <Button variant="destructive" className="w-[100px] p-1">delete</Button>
-        </CardFooter>
-      </Card>
+      <AlertDialog>
+        <Card className=" w-[600px]">
+          <CardHeader>
+            <CardTitle>{title}</CardTitle>
+          </CardHeader>
+          <CardContent>{description}</CardContent>
+          <CardFooter className="flex w-full justify-between my-2">
+            <Button variant="secondary" className="w-[100px] p-1">
+              view
+            </Button>
+            {button}
+            <AlertDialogTrigger asChild>
+              <Button variant="destructive" className="w-[100px] p-1">
+                Delete
+              </Button>
+            </AlertDialogTrigger>
+          </CardFooter>
+        </Card>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 };
--- src/views/list.tsx
+++ src/views/list.tsx
@@ -1,32 +1,95 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import ListCard from "@/components/list-card";
+import { useStoreActions } from "@/hooks/useEasyPeasy";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 const List = (): React.JSX.Element => {
+  const { fetchHome_Posts, fetchLibrary_Posts } = useStoreActions(
+    (actions) => actions
+  );
+
+  React.useEffect(() => {
+    fetchHome_Posts();
+    fetchLibrary_Posts();
+  }, []);
+
   return (
     <div className=" flex flex-col items-center justify-center w-full h-full">
-      <Tabs
-        defaultValue="home"
-        className="w-full flex flex-col items-center justify-start pb-12 min-h-[400px]"
-      >
-        <TabsList className="mb-10">
-          <TabsTrigger value="home">Home Posts</TabsTrigger>
-          <TabsTrigger value="library">Library</TabsTrigger>
-          <TabsTrigger value="blog">Blog post</TabsTrigger>
-        </TabsList>
-        <TabsContent value="home">
-          <ListCard title="home" description="home post" />
-          <ListCard title="home" description="home post"/>
-        </TabsContent>
-        <TabsContent value="library">
-          <ListCard title="home" description="home post" />
-          <ListCard title="home" description="home post"/>
-        </TabsContent>
-        <TabsContent value="blog">
-          <ListCard title="home" description="home post" />
-          <ListCard title="home" description="home post"/>
-        </TabsContent>
-      </Tabs>
+      <Sheet>
+        <Tabs
+          defaultValue="home"
+          className="w-full flex flex-col items-center justify-start pb-12 min-h-[400px]"
+        >
+          <TabsList className="mb-10 fixed">
+            <TabsTrigger value="home">Home Posts</TabsTrigger>
+            <TabsTrigger value="library">Library</TabsTrigger>
+            <TabsTrigger value="blog">Blog post</TabsTrigger>
+          </TabsList>
+          <TabsContent value="home">
+            <ListCard
+              title="home"
+              description="home post"
+              button={
+                <SheetTrigger asChild>
+                  <Button variant="outline" className="w-[100px] p-1">Edit</Button>
+                </SheetTrigger>
+              }
+            />
+            <ListCard title="home" description="home post" />
+            <ListCard title="home" description="home post" />
+            <ListCard title="home" description="home post" />
+            <ListCard title="home" description="home post" />
+          </TabsContent>
+          <TabsContent value="library">
+            <ListCard title="home" description="home post" />
+            <ListCard title="home" description="home post" />
+          </TabsContent>
+          <TabsContent value="blog">
+            <ListCard title="home" description="home post" />
+            <ListCard title="home" description="home post" />
+          </TabsContent>
+        </Tabs>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Edit profile</SheetTitle>
+            <SheetDescription>
+              Make changes to your profile here. Click save when you're done.
+            </SheetDescription>
+          </SheetHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Name
+              </Label>
+              <Input id="name" value="Pedro Duarte" className="col-span-3" />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="username" className="text-right">
+                Username
+              </Label>
+              <Input id="username" value="@peduarte" className="col-span-3" />
+            </div>
+          </div>
+          <SheetFooter>
+            <SheetClose asChild>
+              <Button type="submit">Save changes</Button>
+            </SheetClose>
+          </SheetFooter>
+        </SheetContent>
+      </Sheet>
     </div>
   );
 };
