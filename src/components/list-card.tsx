a644a0e5dcd2c433c3e66cbc47596837df789f83
Milton Black <arziblack2@gmail.com>
2024-12-19 12:01:33 +0100

    feat: updated sheet to drawer...

--- src/components/list-card.tsx
+++ src/components/list-card.tsx
@@ -33,6 +33,7 @@ import {
   DrawerTrigger,
 } from "@/components/ui/drawer";
 import { Minus, Plus } from "lucide-react";
+import { Sheet } from "@/components/ui/sheet";
 
 interface IListCard {
   id: string;
@@ -81,120 +82,121 @@ const ListCard: React.FC<IListCard> = ({
 
   return (
     <div className="w-full mb-2">
-      <Drawer>
-        <AlertDialog>
-          <Card className=" w-[600px]">
-            <CardHeader>
-              <CardTitle>{title}</CardTitle>
-            </CardHeader>
-            <CardContent>
-              {description}
-              {video && (
-                <div className="flex flex-col mt-4 w-full space-y-1.5">
-                  <Label htmlFor="videoUrl" className="text-left">
-                    YouTube Video URL
-                  </Label>
-                  <Input
-                    id="videoUrl"
-                    placeholder="Paste YouTube video URL here"
-                    value={video}
-                    onChange={handleVideoUrlChange}
-                  />
-                  {video_id ||
-                    (video && (
-                      <div className="mt-4 w-full flex justify-center">
-                        <iframe
-                          width="100%"
-                          height="315"
-                          src={`https://www.youtube.com/embed/${video}`}
-                          title="YouTube video preview"
-                          frameBorder="0"
-                          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                          allowFullScreen
-                          className="rounded-lg shadow-md"
-                        ></iframe>
-                      </div>
-                    ))}
-                </div>
-              )}
-            </CardContent>
-            <CardFooter className="flex w-full justify-between my-2">
-              <DrawerTrigger asChild>
-                <Button variant="secondary" className="w-[100px] p-1">
-                  View
-                </Button>
-              </DrawerTrigger>
-              {button}
-              <AlertDialogTrigger asChild>
-                <Button variant="destructive" className="w-[100px] p-1">
-                  Delete
-                </Button>
-              </AlertDialogTrigger>
-            </CardFooter>
-          </Card>
-          <AlertDialogContent>
-            <AlertDialogHeader>
-              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-              <AlertDialogDescription>
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-              <AlertDialogCancel>Cancel</AlertDialogCancel>
-              <AlertDialogAction
-                disabled={loading}
-                onClick={() => {
-                  console.log("Delete button clicked");
-                  handleDelete();
-                }}
-              >
-                {loading ? <Loader2 className="animate-spin" /> : "Continue"}
-              </AlertDialogAction>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
-        <DrawerContent>
-          <div className="mx-auto w-full max-w-sm">
-            <DrawerHeader>
-              <DrawerTitle>Move Goal</DrawerTitle>
-              <DrawerDescription>
-                Set your daily activity goal.
-              </DrawerDescription>
-            </DrawerHeader>
-            <div className="p-4 pb-0">
-              <div className="flex items-center justify-center space-x-2">
-                <Button
-                  variant="outline"
-                  size="icon"
-                  className="h-8 w-8 shrink-0 rounded-full"
-                  onClick={() => onClick(-10)}
-                  disabled={goal <= 200}
-                >
-                  <Minus />
-                  <span className="sr-only">Decrease</span>
-                </Button>
-                <div className="flex-1 text-center">
-                  <div className="text-7xl font-bold tracking-tighter">
-                    {goal}
-                  </div>
-                  <div className="text-[0.70rem] uppercase text-muted-foreground">
-                    Calories/day
-                  </div>
-                </div>
-                <Button
-                  variant="outline"
-                  size="icon"
-                  className="h-8 w-8 shrink-0 rounded-full"
-                  onClick={() => onClick(10)}
-                  disabled={goal >= 400}
-                >
-                  <Plus />
-                  <span className="sr-only">Increase</span>
-                </Button>
+      {/* <Sheet> */}
+      {/* <Drawer> */}
+      <AlertDialog>
+        <Card className=" w-[600px]">
+          <CardHeader>
+            <CardTitle>{title}</CardTitle>
+          </CardHeader>
+          <CardContent>
+            {description}
+            {video && (
+              <div className="flex flex-col mt-4 w-full space-y-1.5">
+                <Label htmlFor="videoUrl" className="text-left">
+                  YouTube Video URL
+                </Label>
+                <Input
+                  id="videoUrl"
+                  placeholder="Paste YouTube video URL here"
+                  value={video}
+                  onChange={handleVideoUrlChange}
+                />
+                {video_id ||
+                  (video && (
+                    <div className="mt-4 w-full flex justify-center">
+                      <iframe
+                        width="100%"
+                        height="315"
+                        src={`https://www.youtube.com/embed/${video}`}
+                        title="YouTube video preview"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                        className="rounded-lg shadow-md"
+                      ></iframe>
+                    </div>
+                  ))}
               </div>
-              <div className="mt-3 h-[120px]">
-                {/* <ResponsiveContainer width="100%" height="100%">
+            )}
+          </CardContent>
+          <CardFooter className="flex w-full justify-between my-2">
+            <DrawerTrigger asChild>
+              <Button variant="secondary" className="w-[100px] p-1">
+                View
+              </Button>
+            </DrawerTrigger>
+            {button}
+            <AlertDialogTrigger asChild>
+              <Button variant="destructive" className="w-[100px] p-1">
+                Delete
+              </Button>
+            </AlertDialogTrigger>
+          </CardFooter>
+        </Card>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction
+              disabled={loading}
+              onClick={() => {
+                console.log("Delete button clicked");
+                handleDelete();
+              }}
+            >
+              {loading ? <Loader2 className="animate-spin" /> : "Continue"}
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+      {/* <DrawerContent>
+            <div className="mx-auto w-full max-w-sm">
+              <DrawerHeader>
+                <DrawerTitle>Edit Goal</DrawerTitle>
+                <DrawerDescription>
+                  Set your daily activity goal.
+                </DrawerDescription>
+              </DrawerHeader>
+              <div className="p-4 pb-0">
+                <div className="flex items-center justify-center space-x-2">
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    className="h-8 w-8 shrink-0 rounded-full"
+                    onClick={() => onClick(-10)}
+                    disabled={goal <= 200}
+                  >
+                    <Minus />
+                    <span className="sr-only">Decrease</span>
+                  </Button>
+                  <div className="flex-1 text-center">
+                    <div className="text-7xl font-bold tracking-tighter">
+                      {goal}
+                    </div>
+                    <div className="text-[0.70rem] uppercase text-muted-foreground">
+                      Calories/day
+                    </div>
+                  </div>
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    className="h-8 w-8 shrink-0 rounded-full"
+                    onClick={() => onClick(10)}
+                    disabled={goal >= 400}
+                  >
+                    <Plus />
+                    <span className="sr-only">Increase</span>
+                  </Button>
+                </div>
+                <div className="mt-3 h-[120px]"> */}
+      {/* <ResponsiveContainer width="100%" height="100%">
                   <BarChart data={data}>
                     <Bar
                       dataKey="goal"
@@ -207,17 +209,17 @@ const ListCard: React.FC<IListCard> = ({
                     />
                   </BarChart>
                 </ResponsiveContainer> */}
-              </div>
-            </div>
-            <DrawerFooter>
-              <Button>Submit</Button>
-              <DrawerClose asChild>
-                <Button variant="outline">Cancel</Button>
-              </DrawerClose>
-            </DrawerFooter>
-          </div>
-        </DrawerContent>
-      </Drawer>
+      {/*</div> */}
+      {/* <DrawerFooter>
+                <Button>Submit</Button>
+                <DrawerClose asChild>
+                  <Button variant="outline">Cancel</Button>
+                </DrawerClose>
+              </DrawerFooter> */}
+      {/* </div> */}
+      {/* </DrawerContent> */}
+      {/* </Drawer> */}
+      {/*</Sheet>*/}
     </div>
   );
 };
--- src/views/list.tsx
+++ src/views/list.tsx
@@ -15,6 +15,17 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Minus, Plus } from "lucide-react";
 
 const List = (): React.JSX.Element => {
   const [view, setView] = React.useState<string>("home");
@@ -26,7 +37,7 @@ const List = (): React.JSX.Element => {
   );
   const { home_post, library_post, loading } = useStoreState((state) => state);
 
-  console.log(view)
+  console.log(view);
   console.log(home_post);
   console.log(library_post);
   React.useEffect(() => {
@@ -36,179 +47,241 @@ const List = (): React.JSX.Element => {
 
   return (
     <div className=" flex flex-col items-center justify-center w-full h-full">
-      <Sheet>
-        <Tabs
-          defaultValue="home"
-          onValueChange={(value) => setView(value)}
-          className="w-full flex flex-col items-center justify-start pb-12 min-h-[400px]"
-        >
-          <TabsList className="mb-10 fixed">
-            <TabsTrigger value="home">Home Posts</TabsTrigger>
-            <TabsTrigger value="library">Library</TabsTrigger>
-            <TabsTrigger value="blog">Blog post</TabsTrigger>
-          </TabsList>
-          <TabsContent value="home">
-            {home_post?.map((item, idx) => (
+      <Drawer>
+        <Sheet>
+          <Tabs
+            defaultValue="home"
+            onValueChange={(value) => setView(value)}
+            className="w-full flex flex-col items-center justify-start pb-12 min-h-[400px]"
+          >
+            <TabsList className="mb-10 fixed">
+              <TabsTrigger value="home">Home Posts</TabsTrigger>
+              <TabsTrigger value="library">Library</TabsTrigger>
+              <TabsTrigger value="blog">Blog post</TabsTrigger>
+            </TabsList>
+            <TabsContent value="home">
+              {home_post?.map((item, idx) => (
+                <ListCard
+                  key={idx}
+                  view={view}
+                  id={item?._id}
+                  title={item?.title}
+                  description={item?.description}
+                  button={
+                    <SheetTrigger asChild>
+                      <Button variant="secondary" className="w-[100px] p-1">
+                        Edit
+                      </Button>
+                    </SheetTrigger>
+                  }
+                />
+              ))}
+              <SheetContent>
+                <SheetHeader>
+                  <SheetTitle>Edit home</SheetTitle>
+                  <SheetDescription>
+                    Make changes to your home post here. Click save when you're
+                    done.
+                  </SheetDescription>
+                </SheetHeader>
+                <div className="grid gap-4 py-4">
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="name" className="text-right">
+                      Name
+                    </Label>
+                    <Input
+                      id="name"
+                      value="Pedro Duarte"
+                      className="col-span-3"
+                    />
+                  </div>
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="username" className="text-right">
+                      Username
+                    </Label>
+                    <Input
+                      id="username"
+                      value="@peduarte"
+                      className="col-span-3"
+                    />
+                  </div>
+                </div>
+                <SheetFooter>
+                  <SheetClose asChild>
+                    <Button type="submit">Save changes</Button>
+                  </SheetClose>
+                </SheetFooter>
+              </SheetContent>
+            </TabsContent>
+            <TabsContent value="library">
+              {library_post?.map((item, idx) => (
+                <ListCard
+                  key={idx}
+                  view={view}
+                  id={item?._id}
+                  title={item?.title}
+                  video={item?.video_id as string}
+                  button={
+                    <SheetTrigger asChild>
+                      <Button variant="secondary" className="w-[100px] p-1">
+                        Edit
+                      </Button>
+                    </SheetTrigger>
+                  }
+                />
+              ))}
+              <SheetContent>
+                <SheetHeader>
+                  <SheetTitle>Edit library</SheetTitle>
+                  <SheetDescription>
+                    Make changes to your library post here. Click save when
+                    you're done.
+                  </SheetDescription>
+                </SheetHeader>
+                <div className="grid gap-4 py-4">
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="name" className="text-right">
+                      Name
+                    </Label>
+                    <Input
+                      id="name"
+                      value="Pedro Duarte"
+                      className="col-span-3"
+                    />
+                  </div>
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="username" className="text-right">
+                      Username
+                    </Label>
+                    <Input
+                      id="username"
+                      value="@peduarte"
+                      className="col-span-3"
+                    />
+                  </div>
+                </div>
+                <SheetFooter>
+                  <SheetClose asChild>
+                    <Button type="submit">Save changes</Button>
+                  </SheetClose>
+                </SheetFooter>
+              </SheetContent>
+            </TabsContent>
+            <TabsContent value="blog">
               <ListCard
-                key={idx}
+                id="5"
+                title={view}
                 view={view}
-                id={item?._id}
-                title={item?.title}
-                description={item?.description}
+                description="home post"
                 button={
                   <SheetTrigger asChild>
-                    <Button variant="secondary" className="w-[100px] p-1">
+                    <Button variant="outline" className="w-[100px] p-1">
                       Edit
                     </Button>
                   </SheetTrigger>
                 }
               />
-            ))}
-            <SheetContent>
-              <SheetHeader>
-                <SheetTitle>Edit home</SheetTitle>
-                <SheetDescription>
-                  Make changes to your home post here. Click save when you're
-                  done.
-                </SheetDescription>
-              </SheetHeader>
-              <div className="grid gap-4 py-4">
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="name" className="text-right">
-                    Name
-                  </Label>
-                  <Input
-                    id="name"
-                    value="Pedro Duarte"
-                    className="col-span-3"
-                  />
-                </div>
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="username" className="text-right">
-                    Username
-                  </Label>
-                  <Input
-                    id="username"
-                    value="@peduarte"
-                    className="col-span-3"
-                  />
-                </div>
-              </div>
-              <SheetFooter>
-                <SheetClose asChild>
-                  <Button type="submit">Save changes</Button>
-                </SheetClose>
-              </SheetFooter>
-            </SheetContent>
-          </TabsContent>
-          <TabsContent value="library">
-            {library_post?.map((item, idx) => (
               <ListCard
-                key={idx}
-                view={view}
-                id={item?._id}
-                title={item?.title}
-                video={item?.video_id as string}
+                id="6"
+                title={view}
+                description="blog post"
                 button={
-                  <SheetTrigger asChild>
+                  <DrawerTrigger asChild>
                     <Button variant="secondary" className="w-[100px] p-1">
                       Edit
                     </Button>
-                  </SheetTrigger>
+                  </DrawerTrigger>
                 }
               />
-            ))}
-            <SheetContent>
-              <SheetHeader>
-                <SheetTitle>Edit library</SheetTitle>
-                <SheetDescription>
-                  Make changes to your library post here. Click save when you're
-                  done.
-                </SheetDescription>
-              </SheetHeader>
-              <div className="grid gap-4 py-4">
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="name" className="text-right">
-                    Name
-                  </Label>
-                  <Input
-                    id="name"
-                    value="Pedro Duarte"
-                    className="col-span-3"
-                  />
+              {/* <SheetContent>
+                <SheetHeader>
+                  <SheetTitle>Edit library</SheetTitle>
+                  <SheetDescription>
+                    Make changes to your library post here. Click save when
+                    you're done.
+                  </SheetDescription>
+                </SheetHeader>
+                <div className="grid gap-4 py-4">
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="name" className="text-right">
+                      hello
+                    </Label>
+                    <Input
+                      id="name"
+                      value="Pedro Duarte"
+                      className="col-span-3"
+                    />
+                  </div>
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="username" className="text-right">
+                      Username
+                    </Label>
+                    <Input
+                      id="username"
+                      value="@peduarte"
+                      className="col-span-3"
+                    />
+                  </div>
                 </div>
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="username" className="text-right">
-                    Username
-                  </Label>
-                  <Input
-                    id="username"
-                    value="@peduarte"
-                    className="col-span-3"
-                  />
+                <SheetFooter>
+                  <SheetClose asChild>
+                    <Button type="submit">Save changes</Button>
+                  </SheetClose>
+                </SheetFooter>
+              </SheetContent> */}
+              <DrawerContent>
+                <div className="mx-auto w-full max-w-sm">
+                  <DrawerHeader>
+                    <DrawerTitle>Edit Goal</DrawerTitle>
+                    <DrawerDescription>
+                      Set your daily activity goal.
+                    </DrawerDescription>
+                  </DrawerHeader>
+                  <div className="p-4 pb-0">
+                    <div className="flex items-center justify-center space-x-2">
+                      <Button
+                        variant="outline"
+                        size="icon"
+                        className="h-8 w-8 shrink-0 rounded-full"
+                        onClick={() => {}}
+                        disabled={false}
+                      >
+                        <Minus />
+                        <span className="sr-only">Decrease</span>
+                      </Button>
+                      <div className="flex-1 text-center">
+                        <div className="text-7xl font-bold tracking-tighter">
+                          {"goal"}
+                        </div>
+                        <div className="text-[0.70rem] uppercase text-muted-foreground">
+                          Calories/day
+                        </div>
+                      </div>
+                      <Button
+                        variant="outline"
+                        size="icon"
+                        className="h-8 w-8 shrink-0 rounded-full"
+                        onClick={() => {}}
+                        disabled={true}
+                      >
+                        <Plus />
+                        <span className="sr-only">Increase</span>
+                      </Button>
+                    </div>
+                    <div className="mt-3 h-[120px]"></div>
+                  </div>
+                  <DrawerFooter>
+                    <Button>Submit</Button>
+                    <DrawerClose asChild>
+                      <Button variant="outline">Cancel</Button>
+                    </DrawerClose>
+                  </DrawerFooter>
                 </div>
-              </div>
-              <SheetFooter>
-                <SheetClose asChild>
-                  <Button type="submit">Save changes</Button>
-                </SheetClose>
-              </SheetFooter>
-            </SheetContent>
-          </TabsContent>
-          <TabsContent value="blog">
-            <ListCard
-              id="5"
-              title={view}
-              view={view}
-              description="home post"
-              button={
-                <SheetTrigger asChild>
-                  <Button variant="secondary" className="w-[100px] p-1">
-                    Edit
-                  </Button>
-                </SheetTrigger>
-              }
-            />
-            <ListCard id="6" title={view} description="home post" />
-            <SheetContent>
-              <SheetHeader>
-                <SheetTitle>Edit library</SheetTitle>
-                <SheetDescription>
-                  Make changes to your library post here. Click save when you're
-                  done.
-                </SheetDescription>
-              </SheetHeader>
-              <div className="grid gap-4 py-4">
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="name" className="text-right">
-                    Name
-                  </Label>
-                  <Input
-                    id="name"
-                    value="Pedro Duarte"
-                    className="col-span-3"
-                  />
-                </div>
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="username" className="text-right">
-                    Username
-                  </Label>
-                  <Input
-                    id="username"
-                    value="@peduarte"
-                    className="col-span-3"
-                  />
-                </div>
-              </div>
-              <SheetFooter>
-                <SheetClose asChild>
-                  <Button type="submit">Save changes</Button>
-                </SheetClose>
-              </SheetFooter>
-            </SheetContent>
-          </TabsContent>
-        </Tabs>
-      </Sheet>
+              </DrawerContent>
+            </TabsContent>
+          </Tabs>
+        </Sheet>
+      </Drawer>
     </div>
   );
 };
